4496814830258fd7a94e4d6a34cbdf6e39bde622
Christopher Rizzo <christophermatthewrizzo@gmail.com>
2018-12-11 01:34:19 -0500

    Day 08 ( slight refactor to remove unneeded print statements )

--- Day08_Memory_Maneuver/01_get_metadata.py
+++ Day08_Memory_Maneuver/01_get_metadata.py
@@ -9,45 +9,29 @@ all_metadata = []
 
 # Problem 1 - returns list of reaction string after performing all reactions
 def get_meta_data(input): 
-    print(input)
     data = input.split()
     # start a tree that will parse all children until there is no data left
     start_node = Node(data)
-    # pprint(all_metadata)
-
     metadata_sum = reduce( (lambda total_sum, curr: total_sum + curr), all_metadata )
-    # return metadata_sum
+    # Problem 1 return metadata_sum
+    # Problem 2 - get root node value
     return start_node.get_node_value()
-    # starting with the start_node, go through all children and get their sums
-
-
 
 class Node:
 
     total_nodes = 0
 
     def __init__(self, data):
+        # Start Children with empty placeholder to be able to directly plug in metadata indexes for problem 2
         self.children = [ None ]
         self.metadata = []
         self.child_count = int(data.pop(0))
         self.metadata_count = int(data.pop(0))
         Node.total_nodes += 1
         self.node_id = Node.total_nodes
-        # print ('processing node number', self.node_id)
-  
-        # print ('found child count and metadata for node: ', self.node_id, self.child_count, self.metadata_count)
-        # print(len(data))
+ 
         if (len(data) > 0):
             self.process_remaining_nodes(data)
-        # print ("Done with node", self.node_id)
-        # print(self)
-        # TODO: Calculate 'value' of this node 
-            # go get the value of all children
-                # metadata is reference to index of where the child is
-                # so plug in the metadata - 1 to children array to find its child
-                # or we can initiate children array with None 
-                # if a node has no children, its value is the sum of its metadata
-        pass
     
     def __str__(self):
         return "child_count " + str(self.child_count) + " " + str(len(self.children)) + " metadata_count " + str(self.metadata_count) + " " + str(len(self.metadata))
@@ -56,33 +40,25 @@ class Node:
         global all_metadata
         # while there is a child count, process the child 
         while len(self.children) <= self.child_count and len(data):
-            # print ('making child for node id: ', self.node_id)
             node = Node(data)
             self.children.append(node)
         # then update metadata
         while (len(self.metadata) < self.metadata_count):
-            # print ('pre metadata removal', len(data))
             self.metadata.append(int(data.pop(0)))
-        #     print ('post metadata removal', len(data))
-        # print ('found metadata for node: ', self.node_id)
-        pprint(self.metadata)
-        pprint(self.children)
         all_metadata.extend(self.metadata)
-        # print ('child length and children', self.child_count, len(self.children))
+
     def get_node_value(self):
         value = 0
-        # found_child = False
+        # if no children, a node's value is the sum of its metadata
         if (len(self.children) == 1):
             return sum(self.metadata)
-        # loop through metadata
+        # else the node should use its metadata as indexes of its children
+        # and if a child node exists at that index, it should aggregate the indexes
         for index in self.metadata:
-            # for each metadata, if child node exists at that index
+            # for each metadata, if child node exists at that index - get its child's value
             if index < len(self.children):
                 child = self.children[index]
-                # found_child = True 
-                # aggregate its value
-                print('getting node value of child', child.node_id)
-                value = value +  child.get_node_value()
+                value +=  child.get_node_value()
         return value
 
 
@@ -94,6 +70,6 @@ def get_file_lines():
 
 if __name__ == "__main__":
     input = get_file_lines()[0]
-    # Testing
+    # Uncomment for test Input
     # input = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
     print ('Result: ', get_meta_data(input))
